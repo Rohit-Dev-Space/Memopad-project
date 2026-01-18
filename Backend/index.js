f58ea15c4beda93a301910a62dfc62868bdffccf
Rohit kharvi <you@example.com>
2026-01-18 19:12:51 +0530

    ready for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -12,16 +12,17 @@ const PORT = process.env.PORT || 5000;
 
 const express = require('express')
 const app = express()
-const cors = require({
-    origin: '*',
-    methods: ['GET', 'PUT', 'DELETE', 'POST'],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-})
+const cors = require('cors')
 const bcrypt = require('bcrypt')
 
 mongoose.connect(process.env.MONGO_URI)
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'POST'],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}))
 const jwt = require('jsonwebtoken')
 const sentNoteModel = require('./models/sent-note.model')
 app.use(express.json())
